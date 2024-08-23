8d75ea4711f6e2df4768a6828e2ee4c987950e39
Sean McKenzie <70048908+seanmckenzie428@users.noreply.github.com>
2024-08-23 09:56:52 -0600

    Add testGenerateCodeWithDashes() and testGenerateCodes()

--- Tests/codeforgeTests/codeforgeTests.swift
+++ Tests/codeforgeTests/codeforgeTests.swift
@@ -8,4 +8,21 @@ final class codeforgeTests: XCTestCase {
         let code = codeforge.generateCode()
         XCTAssertEqual(code.count, 10)
     }
+
+    func testGenerateCodeWithDashes() {
+        let parameters = CodeforgeParameters(numOfCodes: 1, codeLength: 15, charactersBetweenDashes: 5)
+        let codeforge = Codeforge(parameters)
+        let code = codeforge.generateCode()
+        let chunks = code.split(separator: "-")
+
+        XCTAssertEqual(chunks.count, 3)
+        XCTAssertEqual(chunks[0].count, 5)
+    }
+
+    func testGenerateCodes() {
+        let parameters = CodeforgeParameters(numOfCodes: 10, codeLength: 10, charactersBetweenDashes: 100)
+        let codeforge = Codeforge(parameters)
+        let codes = codeforge.generateCodes(parameters)
+        XCTAssertEqual(codes.count, 10)
+    }
 }
